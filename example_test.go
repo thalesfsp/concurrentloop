c769242ae8db8502ac2fca08ee1eb50ff21b5f3f
Energy Star <thalesfsp@users.noreply.github.com>
2023-02-13 17:28:54 -0800

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -1,4 +1,6 @@
 // Copyright 2022 The concurrentloop Authors. All rights reserved.
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
+//
+// SEE: concurrentloop_test.go
 package concurrentloop_test
